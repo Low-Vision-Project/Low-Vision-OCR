a2a4c2c1530784a3fa1d4330720305ce853f5920
AhmetHamdiCengiz <66183595+AhmetHamdiCengiz@users.noreply.github.com>
2021-05-14 18:17:23 +0300

    Fix Splash Screen Name

--- app/src/main/java/com/lowvision/ocr/SplashScreen.kt
+++ app/src/main/java/com/lowvision/ocr/SplashScreen.kt
@@ -3,7 +3,7 @@ package com.lowvision.ocr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Splash_Screen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
